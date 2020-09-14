8c88be80799e400b30da5d471a3d3580078fc9b4
Samuel Gomez Palomaki <gressboldude@gmail.com>
2020-09-14 19:30:29 +0200

    added table lib

--- src/components/cell/data-view/dataTableView.tsx
+++ src/components/cell/data-view/dataTableView.tsx
@@ -1,12 +1,13 @@
 import React, { FunctionComponent } from 'react'
 import * as df from 'data-forge'
+import DataTable from 'react-data-table-component'
 
 const DataTableView: FunctionComponent<any> = ({ dataset }) => {
   const jsonData = new df.DataFrame(dataset).toJSON()
   const data = JSON.parse(jsonData)
 
   const columns = Object.keys(data[0]).map((item) => {
-    return { title: item }
+    return { name: item, selector: item }
   })
   const rows = data.map((el: any, i: number) => {
     return Object.values(el).map((item, i) => item)
@@ -15,6 +16,12 @@ const DataTableView: FunctionComponent<any> = ({ dataset }) => {
   return (
     <div>
       <h1>Table View!</h1>
+      <DataTable
+        fixedHeader={true}
+        columns={columns}
+        data={data}
+        dense={true}
+      />
     </div>
   )
 }
