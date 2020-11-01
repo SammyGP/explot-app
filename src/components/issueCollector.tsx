2979780d7fa311ecc0667a392641b13cd78ac95e
Samuel Gomez Palomaki <gressboldude@gmail.com>
2020-11-01 11:37:15 +0100

    added issue collector endpoint

--- src/components/issueCollector.tsx
+++ src/components/issueCollector.tsx
@@ -30,12 +30,26 @@ const IssueCollector: FunctionComponent<any> = ({ toggleForm }) => {
         onSubmit={(e) => {
           e.preventDefault()
           const user = firebase.auth.currentUser?.email
-          toggleForm(false)
-          alert('Thank you for your feedback')
+          fetch('https://getform.io/f/a21fd6eb-a8be-4219-b236-cff9c29c2972', {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify({ user, contet: issue }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          })
+            .then(() => {
+              toggleForm(false)
+              alert('Thank you for your feedback')
+            })
+            .catch((err) => {
+              alert(`An error occured ${err}`)
+            })
         }}
       >
         <label>Describe the Issue</label>
         <textarea
+          name='content'
           value={issue}
           onChange={(e) => updateIssue(e.target.value)}
         ></textarea>
